9e43b4e522a45e6323895c342005ea22696b5200
frank <f.dersjant@inholland.nl>
2021-05-14 12:20:50 +0200

    states

--- ConsoleAppState/States/Sleeping.cs
+++ ConsoleAppState/States/Sleeping.cs
@@ -5,22 +5,12 @@ namespace ConsoleAppState.States
     public class Sleeping : CatState
     {
         public Sleeping(CatContext Handle) : base(Handle)
-        {
-
-        }
-
-        public override void BowlEmpty()
-        {
-            throw new NotImplementedException();
-        }
-
+        {}
 
         public override void GetsFed(int amount, int HappyWith)
         {
             Console.WriteLine("Hmm lekker!");
             this.Handle.CurrentState = new Eating(this.Handle, amount, HappyWith);
-
         }
-
     }
 }
