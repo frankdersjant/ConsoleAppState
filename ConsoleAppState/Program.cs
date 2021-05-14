ee8f92b7259f25c4c129b4c4b8ba1d3b1d44342a
frank <f.dersjant@inholland.nl>
2021-05-14 09:18:53 +0200

    Add project files.

---
+++ ConsoleAppState/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ConsoleAppState
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //minimaize duplication of code
+            //decoupling of code/ca
+            //swap algirithims at runtime - without the use of if and/or switch statements
+
+        }
+    }
+}
